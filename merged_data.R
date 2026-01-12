ef6171384ad9e879448dc010bcf4ad4bae7ef73d
MooreAx <moore.ax@gmail.com>
2026-01-11 20:36:31 -0600

    updated summary calculations

--- merged_data.R
+++ merged_data.R
@@ -5,7 +5,7 @@
 
 library(tidyverse)
 library(lubridate)
-library(gglplot2)
+library(ggplot2)
 
 #build schedule - analyze_hourly_tafs already has everything
 
@@ -15,35 +15,49 @@ window_end = as_datetime(ymd(20251031))+hours(23)
 
 yyq_analysis <- crossing(
   station = "CYYQ",
-  time = seq(window_start, window_end, by = "1 hour"),
+  time = seq(window_start, window_end, by = "1 hour")
   ) %>%
   left_join(
-    metars_with_requirements %>% select(station, issued_time, raw, wind_reconstructed, vis, sigwx, clouds, ceiling, usable_runways, lowest_haa, lowest_ops_spec_vis, suitable_for_landing),
-    join_by(station),
-    relationship = "many-to-many"
-  ) %>%
-  filter(
-    issued_time <= time
+    metars_with_requirements %>%
+      select(station, issued_time, raw, wind_reconstructed, vis, sigwx, clouds, ceiling, usable_runways, lowest_haa, lowest_apch_ban_vis, suitable_for_landing) %>%
+      #add floor_hr to make join more manageable
+      mutate(issued_time_floor_hr = floor_date(issued_time, unit = "hours")),
+    join_by(station, time == issued_time_floor_hr),
+    relationship = "one-to-many"
   ) %>%
   group_by(station, time) %>%
-  slice_max(order_by = issued_time, n = 1, with_ties = FALSE) %>%
-  ungroup()
+  #slice_max(order_by = issued_time, n = 1, with_ties = FALSE) %>%
+  summarise(
+    #if there is any METAR issued within the hour that is not suitable for landing, whole hour is considered not suitable
+    suitable_for_landing = case_when(
+      any(suitable_for_landing == FALSE, na.rm = TRUE) ~ FALSE,        # any FALSE → FALSE
+      all(suitable_for_landing == TRUE, na.rm = TRUE) & any(is.na(suitable_for_landing)) ~ NA,  # TRUE + NA → NA
+      all(suitable_for_landing == TRUE, na.rm = TRUE) ~ TRUE          # all TRUE → TRUE
+    ),
+    .groups = "drop"
+  )
+
+yyq_suitable_for_landing <- yyq_analysis %>%
+  group_by(station) %>%
+  summarise(
+    suitable_for_landing = mean(as.numeric(suitable_for_landing), na.rm = TRUE)
+  )
 
 
 alt_order <- c("CYGX", "CYTH", "CYYL", "CYNE", "CYQD")
 
 available_alts_wide <- alternate_availability %>% 
   select(station, time, suitable_alternate) %>%
+  filter(station %in% alt_order) %>%
   pivot_wider(
    names_from = station,
    values_from = suitable_alternate
   ) %>%
-  select(-CYYQ) %>%
   relocate(CYGX, CYTH, CYYL, CYNE, CYQD, .after = time)
 
 
 collapsed_alts <- alternate_availability %>%
-  filter(suitable_alternate, station != "CYYQ") %>%
+  filter(suitable_alternate, station %in% alt_order) %>%
   mutate(station = factor(station, levels = alt_order)) %>% # preference order
   group_by(time) %>%
   arrange(station) %>% # ensure preference order
@@ -53,7 +67,6 @@ collapsed_alts <- alternate_availability %>%
     n_alts = n(), # count of available alternates
     .groups = "drop"
   )
-
 
 yyq_analysis2 <- yyq_analysis %>%
   left_join(
@@ -65,11 +78,11 @@ yyq_analysis2 <- yyq_analysis %>%
     collapsed_alts,
     join_by(time),
     relationship = "one-to-one"
-  )
+  ) %>%
+  replace_na(list(n_alts = 0))
 
 #sample data
 yyq_analysis2 %>%
-  select(-c(issued_time:lowest_ops_spec_vis)) %>%
   filter(time>ymd(20251001)) %>%
   write_csv("Oct_2025_Alternate_Availability.csv")
 
@@ -94,3 +107,25 @@ summary_stats <- yyq_analysis2 %>%
 
 write_csv(summary_stats, "summary_stats.csv")
 write_csv(dis_num_alts, "num_alt_dist.csv")
+
+
+
+
+#breakdown of alternate availability, including breakout of unusable
+alt_available_breakdown <- alternate_availability %>%
+  filter(time >= window_start, time <= window_end) %>%
+  mutate(
+    outcome = case_when(
+      suitable_alternate == FALSE & reason == "TAF UNUSABLE" ~ "UNSUITABLE - TAF UNUSABLE",
+      suitable_alternate == FALSE & !(reason == "TAF UNUSABLE") ~ "UNSUITABLE - TAF < ALT MINIMA",
+      suitable_alternate == TRUE ~ "SUITABLE"
+    )
+  ) %>%
+  group_by(station, outcome) %>%
+  summarise(
+    n=n(),
+    .groups = "drop"
+  )
+
+alt_available_breakdown %>% write_csv("alt_available_breakdown.csv")
+

b399723e33232671ab158ea83a11deb03f224292
MooreAx <moore.ax@gmail.com>
2026-01-11 20:35:53 -0600

    fix VRB processing

--- functions.R
+++ functions.R
@@ -5,11 +5,23 @@ calc_max_tailwind <- function(wind_str, rwy_bearing_mag, variation) {
   }
   
   #get a list of winds -- e.g. "12010G20KT -> 15015KT" -> [["12010G20", "15015"]]
-  wind_list <- str_extract_all(wind_str, "\\d{3}\\d{2}(?:G\\d{2})?", simplify = FALSE)[[1]]
+  wind_list <- str_extract_all(
+    wind_str,
+    "(VRB\\d{2}|\\d{3}\\d{2}(?:G\\d{2})?)",
+    simplify = FALSE
+  )[[1]]
   
   if (length(wind_list) == 0) return(NA)
   
   calc_one_tailwind <- function(wind, rwy_bearing, variation) {
+    
+    # --- VRB case (no gusts possible) ---
+    if (str_detect(wind, "^VRB")) {
+      speed <- as.numeric(str_extract(wind, "\\d{2}"))
+      return(speed)
+    }
+    
+    # --- Fixed-direction wind ---
     wind_dir <- as.numeric(str_extract(wind, "^\\d{3}"))
     wind_speed  <- as.numeric(str_extract(wind, "(?<=^[0-9]{3})[0-9]{2}"))
     
